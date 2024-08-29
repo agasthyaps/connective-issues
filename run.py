2eb2ce2a031eb2811cddf9fe1d81ab864533e06c
agasthyaps <agasthya.ps@gmail.com>
2024-08-29 14:09:34 -0400

    more

--- run.py
+++ run.py
@@ -1,6 +1,13 @@
 import os
+from gevent import monkey
+monkey.patch_all()
+
 from app import app, socketio
+from gevent.pywsgi import WSGIServer
+from geventwebsocket.handler import WebSocketHandler
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    socketio.run(app, host='0.0.0.0', port=port)
+    http_server = WSGIServer(('0.0.0.0', port), app, handler_class=WebSocketHandler)
+    print(f"Server is running on port {port}")
+    http_server.serve_forever()
